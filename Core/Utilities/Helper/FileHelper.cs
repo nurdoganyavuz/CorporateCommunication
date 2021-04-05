8fb527abe4a3a0d5f82fa72df1a857faf9603af8
NURDOGAN YAVUZ <nurdogan.yavuz@ogrenci.karatay.edu.tr>
2021-04-06 02:30:14 +0300

    Add project files.

---
+++ Core/Utilities/Helper/FileHelper.cs
@@ -0,0 +1,45 @@
+﻿using Core.Utilities.Results;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Core.Utilities.Helper
+{
+    public class FileHelper
+    {
+        static string directory = Directory.GetCurrentDirectory() + @"\wwwroot\";
+        static string path = @"Images\";
+        public static string Add(IFormFile file)
+        {
+            string extension = Path.GetExtension(file.FileName).ToLower();
+            string newFileName = Guid.NewGuid().ToString("N") + extension;
+            if (!Directory.Exists(directory + path))
+            {
+                Directory.CreateDirectory(directory + path);
+            }
+            using (FileStream fileStream = File.Create(directory + path + newFileName))
+            {
+                file.CopyTo(fileStream);
+                fileStream.Flush();
+            }
+            return (path + newFileName).Replace("\\", "/");
+        }
+
+        public static string Update(IFormFile file, string OldImagePath)
+        {
+            Delete(OldImagePath);
+            return Add(file);
+        }
+
+        public static void Delete(string ImagePath)
+        {
+            if (File.Exists(directory + ImagePath.Replace("/", "\\")) && Path.GetFileName(ImagePath) != "default.jpg")
+            {
+                File.Delete(directory + ImagePath.Replace("/", "\\"));
+            }
+        }
+
+    }
+}
